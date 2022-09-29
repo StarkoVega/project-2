ccbd2cbba3f8eee8e3c69b9aaf8fc6f946b57a0f
StarkoVega <jose.vega.rosas.2002@gmail.com>
2022-09-29 08:50:52 -0700

    Router added

---
+++ src/GroupedTeamMembers.jsx
@@ -0,0 +1,13 @@
+const GroupedTeamMembers = () => {
+  return (
+    <main className="container">
+      <div className="row">
+        <div className="col-8">
+          <h1>aaaa</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default GroupedTeamMembers;
